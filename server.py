176cd36bd2bf271456d49127035735ca9fcfa72b
Sam Redmond <sredmond@techusolutions.com>
2019-03-25 12:05:09 -0500

    initial servo logic for moving camera

--- server.py
+++ server.py
@@ -9,6 +9,7 @@ from firebase_admin import auth
 import base64
 import os
 import json
+from gpiozero import PWMOutputDevice
 
 root = os.path.normpath(os.path.dirname(__file__))
 path = os.path.join(root, 'key.json')
@@ -22,9 +23,45 @@ except ImportError:
 
 # SETUP CAMERA
 camera = cv2.VideoCapture(0)
-w,h = 1280, 720
+w, h = 1280, 720
 camera.set(3, w)
 camera.set(4, h)
+
+pin = 23
+
+servo = PWMOutputDevice(pin)
+servo.on()
+
+servo_position = 0
+
+
+def require_firebase_auth(handler_class):
+    def wrap_execute(handler_execute):
+        def serve_error(handler, status):
+            handler._transforms = []  # necessary
+            handler.set_status(status)
+            handler.finish()
+
+        def _execute(self, transforms, *args, **kwargs):
+            token = self.request.headers.get('Authorization')
+
+            if token is None:
+                return serve_error(self, 403)
+
+            token = token.split(" ")[1]
+            decoded_token = auth.verify_id_token(token)
+
+            if decoded_token is None:
+                return serve_error(self, 403)
+            
+            if(decoded_token["authorized"] is True or decoded_token["admin"] is True):
+                return handler_execute(self, transforms, *args, **kwargs)
+
+        return _execute
+
+    handler_class._execute = wrap_execute(handler_class._execute)
+
+    return handler_class
 
 class WebSocket(tornado.websocket.WebSocketHandler):
 
@@ -34,21 +71,16 @@ class WebSocket(tornado.websocket.WebSocketHandler):
     def on_message(self, message):
         # message should be read_{{TOKEN}}
         msg = json.loads(message)
-        print("MESSAGE: ", msg)
         if msg['read']:
             token = msg['token']
             try:
-                print("TOKEN: ", token)
                 decodedToken = auth.verify_id_token(token)
-                print("DECODED: ", decodedToken)
-                if decodedToken['admin'] == True or decodedToken['authorized'] == True :
+                if decodedToken['admin'] is True or decodedToken['authorized'] is True:
                     self.camera_loop = PeriodicCallback(self.loop, 10)
                     self.camera_loop.start()
             except:
                 print("Unauthorized")
-        # self.camera_loop = PeriodicCallback(self.loop, 10)
-        # self.camera_loop.start()
-            
+
     def loop(self):
         sio = io.BytesIO()
 
@@ -56,13 +88,27 @@ class WebSocket(tornado.websocket.WebSocketHandler):
         frame = cv2.flip(frame, 1)
         img = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
         img.save(sio, "JPEG")
-    
+
         try:
             self.write_message(base64.b64encode(sio.getvalue()))
         except tornado.websocket.WebSocketClosedError:
             self.camera_loop.stop()
 
-handlers = [(r"/camera", WebSocket)]
+@require_firebase_auth
+class MoveLeftHandler(tornado.web.RequestHandler):
+    def post(self, **kwargs):
+        servo_position++
+        servo.value = servo_position
+
+@require_firebase_auth
+class MoveRightHandler(tornado.web.RequestHandler):
+    def post(self, **kwargs):
+        servo_position--
+        servo.value = servo_position
+
+
+handlers = [(r"/camera", WebSocket), (r"/right", MoveRightHandler),
+            (r"/left", MoveLeftHandler)]
 
 application = tornado.web.Application(handlers)
 application.listen(8000)
@@ -87,7 +133,7 @@ tornado.ioloop.IOLoop.instance().start()
 #     encoded = base64.b64encode(sio.getvalue())
 
 #     print("ENCODED: ", encoded)
-    
+
 #     cv2.imshow('frame',frame)
 #     if cv2.waitKey(1) & 0xFF == ord('q'):
 #         break
